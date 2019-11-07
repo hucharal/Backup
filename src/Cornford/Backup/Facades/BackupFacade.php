5254d94b9b9a1f588982ad79afe029b876061b6a
Hugo Amaral <hucharal13@gmail.com>
2019-11-07 14:23:25 -0300

    Rename backup facade class

--- src/Cornford/Backup/Facades/BackupFacade.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Cornford\Backup\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-class Backup extends Facade {
-
-	protected static function getFacadeAccessor() { return 'backup'; }
-
-}
